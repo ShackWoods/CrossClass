3532dc6d7ac6f18b5c8ba04e361c986997d46f08
Jordan Conway <jordan_conway@outlook.com>
2026-03-26 19:18:40 +0000

    Fix data parsing

--- src/data_reader.c
+++ src/data_reader.c
@@ -17,6 +17,68 @@ void error_invalid_line(char* const reason_msg){
     exit(1);        
 }
 
+// Parses the left side of a line. The result is stored in result. Moves line up to the first ':'. Return the amount of indentation
+int parse_left(const char** line, char** result) {
+    int nextIndex = 0;
+    int indent = 0;
+
+    while(**line != ':'){
+        //Spaces before the first character are indentation
+        while(**line == ' ' && nextIndex == 0){
+            indent++;
+            (*line)++;
+        }
+
+        if(nextIndex >= MAX_LINE_LENGTH) {
+            error_invalid_line("Exceeded max line length");
+        }
+
+        char currentChar = **line;
+
+        // Left side has to end with ':'
+        if(currentChar == '\n' || currentChar == '\0') {
+            error_invalid_line("Found end of left without finding :");
+        }
+
+        (*result)[nextIndex] = tolower(currentChar);
+        nextIndex++;
+        (*line)++;
+    }
+
+    // Trim trailing whitespace
+    int currIndex = nextIndex -1;
+    while(isspace((*result)[currIndex]))
+    {
+        currIndex--;
+    }
+    (*result)[currIndex+1] = '\0';
+
+    return indent;
+}
+
+void parse_right(const char** line, char** result) {
+    int nextIndex = 0;
+    
+    while (**line != '\n' && **line != '\0') {
+        //Ignore spaces before the first character
+        while(**line == ' ' && nextIndex == 0){
+            (*line)++;
+        }
+
+        (*result)[nextIndex] = **line;
+        nextIndex++;
+        (*line)++;
+    }
+
+    // Trim trailing whitespace
+    int currIndex = nextIndex -1;
+    while(isspace((*result)[currIndex]))
+    {
+        currIndex--;
+    }
+    (*result)[currIndex+1] = '\0';
+}
+
 // Parse a single line (left to right)
 struct Line_Data* parse_line(const char* line){
     /*
@@ -30,67 +92,11 @@ struct Line_Data* parse_line(const char* line){
     struct Line_Data* line_data = (struct Line_Data*)calloc(1, sizeof(struct Line_Data));
     line_data->left = (char*)calloc(MAX_LINE_LENGTH, sizeof(char));
     line_data->right = (char*)calloc(MAX_LINE_LENGTH,sizeof(char));
-    line_data->indentation = 0;
 
-    bool is_left = true;
-    int index = 0; // index of the newest character
-    
-    // Parse character by character
-    for(int i=0; i<MAX_LINE_LENGTH; i++)
-    {
-        char currentChar = *(line + i);
+    line_data->indentation = parse_left(&line, &line_data->left);
+    line++; //Line comes back from parse_left pointing at ':'
 
-        // Handle the end of the line
-        if(currentChar == '\n' || currentChar == '\0'){
-            if(is_left){
-                error_invalid_line("Found end of line without finding ':'");
-            }
-            if(index != -1){ // If right-side is not empty
-                while(isspace(line_data->right[index])){ // Trim trailing whitespace
-                    index--;
-                }
-            }
-            index++;
-            line_data->right[index] = '\0';
-            break;
-        }
-
-        // Handle swaps between left and right
-        if(currentChar == ':'){
-            if(!is_left){
-                error_invalid_line("Found ':' on right side of declaration");
-            }
-            if(index == -1){
-                error_invalid_line("Left is empty");
-            }
-            is_left = false;
-            while(isspace(line_data->left[index])){ // Trim trailing whitespace
-                index--;
-            }
-            index++;
-            line_data->left[index] = '\0';
-            index = -1;
-            continue;
-        }
-
-        // Handle indentation
-        if(isspace(currentChar) && index == -1){
-            if(is_left){
-                line_data->indentation++;
-            }
-            continue;
-        }
-
-        // Store the character
-        index++;
-        if(is_left)
-        {
-            line_data->left[index] = tolower(currentChar);
-        }
-        else{
-            line_data->right[index] = currentChar;
-        }
-    }
+    parse_right(&line, &line_data->right);
 
     return line_data;
 }
