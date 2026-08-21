e9f5a4a15e00573bffdc47364e5bfe5d9885f006
Skyla Wakefield <ldyb96@durham.ac.uk>
2026-08-21 12:37:59 +0100

    Simple Check for Supported Version

--- src/cross_class_lib/src/version.c
+++ src/cross_class_lib/src/version.c
@@ -1,5 +1,8 @@
 #include "../includes/version.h"
 
+#include <stdlib.h>
+#include <stdbool.h>
+
 const struct Version CURRENT_VERSION = {
     .major = 0,
     .minor = 0,
@@ -7,3 +10,23 @@ const struct Version CURRENT_VERSION = {
 };
 
 struct Version get_current_version() { return CURRENT_VERSION; }
+
+bool ensure_version_supported(struct Version *version) {
+    const int majors[5] = { 1, 1, 0, 0, 0 }; //TEMPORARY
+    const int minors[5] = { 1, 0, 1, 1, 0 }; //TEMPORARY
+    const int patches[5] = { 1, 2, 2, 1, 1 }; //TEMPORARY
+
+    for(int i = 0; i < sizeof(majors) / sizeof(majors[0]); i++){
+        if(version->major != majors[i]){
+            continue;
+        }
+        if(version->minor != minors[i]){
+            continue;
+        }
+        if(version->patch != patches[i]){
+            continue;
+        }
+        return true;
+    }
+    return false;
+}
