fefe1732505535b1672812713c00a9dd8a741df9
Skyla Wakefield <ldyb96@durham.ac.uk>
2026-08-25 21:41:41 +0100

    Better approach, using a linked list

--- src/cross_class_lib/src/version.c
+++ src/cross_class_lib/src/version.c
@@ -12,21 +12,63 @@ const struct Version CURRENT_VERSION = {
 struct Version get_current_version() { return CURRENT_VERSION; }
 
 bool ensure_version_supported(struct Version *version) {
-    const int majors[5] = { 1, 1, 0, 0, 0 }; //TEMPORARY
-    const int minors[5] = { 1, 0, 1, 1, 0 }; //TEMPORARY
-    const int patches[5] = { 1, 2, 2, 1, 1 }; //TEMPORARY
+    struct Version_List *supported_versions = get_supported_versions();
 
-    for(int i = 0; i < sizeof(majors) / sizeof(majors[0]); i++){
-        if(version->major != majors[i]){
-            continue;
+    bool valid = false;
+    while(supported_versions != NULL && !valid){
+        if(version->major == supported_versions->data->major &&
+        version->minor == supported_versions->data->minor &&
+        version->patch == supported_versions->data->patch){
+            valid = true;
         }
-        if(version->minor != minors[i]){
-            continue;
-        }
-        if(version->patch != patches[i]){
-            continue;
-        }
-        return true;
+        supported_versions = supported_versions->prev; // As we did not wind back
     }
-    return false;
+
+    // Free up memory
+    delete_list(supported_versions);
+
+    return valid;
+}
+
+// Auxiliary function to minimize space
+void extend_supported_versions(struct Version_List **tail, int major, int minor, int patch) {
+  (*tail)->next = malloc(sizeof(typeof(*((*tail)->next))));
+  struct Version_List *new_version = (*tail)->next;
+  new_version->data = malloc(sizeof(typeof((*new_version->data))));
+  new_version->data->major = major;
+  new_version->data->minor = minor;
+  new_version->data->patch = patch;
+  new_version->next = NULL;
+  new_version->prev = *tail;
+  *tail = new_version;
+}
+
+struct Version_List *get_supported_versions(){
+    struct Version supported_version_data = {
+        .major = 0, .minor = 0, .patch = 0};
+
+    struct Version_List *version_list =
+        malloc(sizeof(typeof(*version_list)));
+    version_list->data = &supported_version_data;
+    version_list->next = NULL;
+    version_list->prev = NULL;
+
+    // Just list out all accepted versions
+    extend_supported_versions(&version_list, 0, 0, 1);
+    return version_list;
+}
+
+void delete_list(struct Version_List *list) {
+  while(list->prev != NULL){
+    list = list->prev;
+  }
+
+  while (list->next != NULL) {
+    delete_data(list->data);
+    list = list->next;
+    free(list->prev);
+  }
+
+  delete_data(list->data);
+  free(list);
 }
